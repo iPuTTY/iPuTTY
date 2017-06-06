94416fb1527e8ce9cd9b72ea7501129dc6d12a4a
JoungKyun Kim <joungkyun@gmail.com>
2017-06-06 22:14:04 +0900

    fixed license and program suit name

--- psftp.c
+++ psftp.c
@@ -2843,7 +2843,7 @@ static void usage(void)
 static void version(void)
 {
   char *buildinfo_text = buildinfo("\n");
-  printf("psftp: %s\n%s\n", ver, buildinfo_text);
+  printf("iPSFTP: %s\n%s\n", ver, buildinfo_text);
   sfree(buildinfo_text);
   exit(0);
 }
--- windows/winpgen.c
+++ windows/winpgen.c
@@ -299,7 +299,7 @@ static INT_PTR CALLBACK AboutProc(HWND hwnd, UINT msg,
         {
             char *buildinfo_text = buildinfo("\r\n");
             char *text = dupprintf
-                ("PuTTYgen\r\n\r\n%s\r\n\r\n%s\r\n\r\n%s",
+                ("iPuTTYgen\r\n\r\n%s\r\n\r\n%s\r\n\r\n%s",
                  ver, buildinfo_text,
                  "(C) " SHORT_COPYRIGHT_DETAILS ". All rights reserved.");
             sfree(buildinfo_text);
@@ -322,7 +322,7 @@ static INT_PTR CALLBACK AboutProc(HWND hwnd, UINT msg,
 	  case 102:
 	    /* Load web browser */
 	    ShellExecute(hwnd, "open",
-			 "http://www.chiark.greenend.org.uk/~sgtatham/putty/",
+			 "https://github.com/iPuTTY/iPuTTY/wiki",
 			 0, 0, SW_SHOWDEFAULT);
 	    return 0;
 	}
